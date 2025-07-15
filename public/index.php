abe557e804b17f90cb4e02fa0b85dc95fc64f271
Sergey Ivanov <ivanitch.mail@gmail.com>
2025-07-15 10:48:24 +0900

    Решение

--- public/index.php
+++ public/index.php
@@ -8,4 +8,28 @@ error_reporting(E_ALL);
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-(new \src\Api())->run();
+
+$api = new \src\Api();
+
+$user =
+    [
+        'id'     => 20,
+        'name'   => 'John Dow',
+        'role'   => 'QA',
+        'salary' => 100
+    ];
+
+$api_path_templates =
+    [
+        "/api/items/%id%/%name%",
+        "/api/items/%id%/%role%",
+        "/api/items/%id%/%salary%"
+    ];
+
+$api_paths = array_map(function ($api_path_template) use ($api, $user) {
+    return $api->get_api_path($user, $api_path_template);
+}, $api_path_templates);
+
+echo json_encode($api_paths, JSON_FORCE_OBJECT | JSON_UNESCAPED_UNICODE);
+
+$expected_result = ['/api/items/20/John%20Dow', '/api/items/20/QA', '/api/items/20/100'];
--- src/Api.php
+++ src/Api.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace src;
 
+/**
+ * Класс для работы с API
+ *
+ * @author      Sergey Ivanov
+ * @version		v.1.0 (15/07/2025)
+ */
 class Api
 {
     public function __construct()
@@ -15,5 +21,28 @@ class Api
         dump(__METHOD__);
 
         dump('Hello, World! | PHP Version: ' . PHP_VERSION);
+    }
+
+    /**
+     * Заполняет строковый шаблон `template` данными из объекта `object`
+     *
+     * @author		Sergey Ivanov
+     * @version		v.1.0 (15/07/2025)
+     * @param		array $array
+     * @param		string $template
+     * @return		string
+     */
+    public function get_api_path(array $array, string $template) : string
+    {
+        return preg_replace_callback('/%(\w+)%/', function ($matches) use ($array) {
+            $key = $matches[1];
+
+            if (!array_key_exists($key, $array)) {
+                return $matches[0];
+            }
+
+            $value = $array[$key];
+            return is_string($value) ? rawurlencode($value) : $value;
+        }, $template);
     }
 }
